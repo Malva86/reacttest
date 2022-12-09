dabfff1461c553c6518fa89a7e4fa1db5544bd50
Malva86 <105357204+Malva86@users.noreply.github.com>
2022-12-09 09:52:34 +0100

    add links

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
         <h1>Weather App</h1>
         <Weather />
       </header>
+      <h3 className="git">
+        en-source code at{" "}
+        <a href="https://github.com/Malva86/reacttest">GitHub</a> and host at{" "}
+        <a href="https://melodious-dodol-603cbe.netlify.app/">Netlify</a>
+      </h3>
     </div>
   );
 }
